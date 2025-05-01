7b4dc6b3a05b6250b1f3b52262545d29445a4cf9
San-tito <68120901+San-tito@users.noreply.github.com>
2025-05-02 01:47:18 +0200

    ssl cert

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -8,21 +8,18 @@ log() {
 }
 
 if [ "$1" = "nginx" ]; then
-	touch /etc/nginx/nginx.conf 2>/dev/null \
+	touch /etc/nginx/nginx.conf >/dev/null 2>&1 \
 		|| { log "error: cannot modify /etc/nginx/nginx.conf"; exit 0; }
 	log "Creating SSL certificate..."
-	mkdir -p /etc/nginx/ssl
+	mkdir -p /etc/nginx/ssl >/dev/null 2>&1 \
+		|| { log "error: cannot create /etc/nginx/ssl"; exit 0; }
 	openssl req -x509 -nodes -days 365 \
 		-newkey rsa:2048 \
 		-keyout /etc/nginx/ssl/nginx.key \
 		-out /etc/nginx/ssl/nginx.crt \
-		-subj "/C=ES/ST=Catalonia/L=Barcelona/O=42Barcelona/OU=42 School/CN=sguzman.42.fr" \
+		-subj "/C=ES/ST=Catalonia/L=Barcelona/O=42Barcelona/OU=42 School/CN=sguzman.42.fr" >/dev/null 2>&1 \
 		|| { log "error: cannot create SSL certificate"; exit 0; }
-    log "Configuration complete; ready for start up"
-	# TIP: If you're not obligated to support ancient clients, remove TLSv1.1.
-	# remove TLSv1.1 in /etc/nginx/nginx.conf ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
-	sed -i 's/ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3/ssl_protocols TLSv1.2 TLSv1.3/g' /etc/nginx/nginx.conf
-	# add server below http { in /etc/nginx/nginx.conf
+	log "Modifying Nginx configuration..."
 	sed -i '/http {/a \
 	server {\
 		listen 443 ssl;\
@@ -30,7 +27,8 @@ if [ "$1" = "nginx" ]; then
 		ssl_certificate /etc/nginx/ssl/nginx.crt;\
 		ssl_certificate_key /etc/nginx/ssl/nginx.key;\
 	}' /etc/nginx/nginx.conf
-	cat /etc/nginx/nginx.conf
+	sed -i 's/ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3/ssl_protocols TLSv1.2 TLSv1.3/g' /etc/nginx/nginx.conf
+	log "Nginx init process completed; ready for start up."
 fi
 
 exec "$@"
