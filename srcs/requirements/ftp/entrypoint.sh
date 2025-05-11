f032b915d686f3aa37017a954192cb6b7c8d07c2
San-tito <68120901+San-tito@users.noreply.github.com>
2025-05-11 14:06:57 +0200

    ftp

--- srcs/requirements/ftp/entrypoint.sh
+++ srcs/requirements/ftp/entrypoint.sh
@@ -19,6 +19,25 @@ verify_minimum_env() {
 
 ftp_init()
 {
+	if [ ! -d "/home/$FTP_USER" ]; then
+		log "Creating FTP user $FTP_USER"
+		addgroup -g 1000 -S $FTP_USER
+		adduser -S -D -H -u 1000 -s /sbin/nologin -G $FTP_USER -g $FTP_USER $FTP_USER
+
+		mkdir -p /home/$FTP_USER
+		chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
+		echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
+	fi
+	if [ ! -f /etc/vsftpd.conf ]; then
+		log "Configuring vsftpd"
+		echo "listen=YES" >> /etc/vsftpd.conf
+		echo "local_enable=YES" >> /etc/vsftpd.conf
+		echo "chroot_local_user=YES" >> /etc/vsftpd.conf
+		echo "anonymous_enable=NO" >> /etc/vsftpd.conf
+		echo "write_enable=YES" >> /etc/vsftpd.conf
+		echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
+		echo "seccomp_sandbox=NO" >> /etc/vsftpd.conf
+	fi
 	log "Ftp Server init process done. Ready for start up."
 }
 
