4e830112ec347f3f74b361fe3ab5dc29e532ceeb
San-tito <68120901+San-tito@users.noreply.github.com>
2025-05-13 10:04:35 +0200

    buenas

--- srcs/requirements/ftp/entrypoint.sh
+++ srcs/requirements/ftp/entrypoint.sh
@@ -12,26 +12,50 @@ error() {
 }
 
 verify_minimum_env() {
-	if [ -z "$FTP_PASS" ]; then
-		error $'Ftp Server is unitialized and options are not specified\n\tYou need to specify FTP_PASS' 
+	if [ -z "$FTP_USER" ] || [ -z "$FTP_PASS" ]; then
+		error $'Ftp Server is unitialized and options are not specified\n\tYou need to specify FTP_USER and FTP_PASS' 
 	fi
 }
 
 ftp_init()
 {
-	echo "ftp:$FTP_PASS" | /usr/sbin/chpasswd
+	addgroup -S "$FTP_USER"
+	adduser -D -G "$FTP_USER" -h "/home/$FTP_USER" -s "/bin/false" "$FTP_USER"
+	mkdir -p /home/$FTP_USER
+	chown -R "$FTP_USER":"$FTP_USER" /home/$FTP_USER
+	echo "$FTP_USER:$FTP_PASS" | chpasswd
 	if [ ! -f /etc/vsftpd.conf ]; then
 		log "Configuring vsftpd"
-		echo "listen=YES" >> /etc/vsftpd.conf
-		echo "local_enable=YES" >> /etc/vsftpd.conf
-		echo "chroot_local_user=YES" >> /etc/vsftpd.conf
-		echo "anonymous_enable=NO" >> /etc/vsftpd.conf
-		echo "write_enable=YES" >> /etc/vsftpd.conf
-		echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
-		echo "seccomp_sandbox=NO" >> /etc/vsftpd.conf
-		echo "pasv_enable=YES" >> /etc/vsftpd.conf
-		echo "pasv_min_port=5000" >> /etc/vsftpd.conf
-		echo "pasv_max_port=5100" >> /etc/vsftpd.conf
+		cat > /etc/vsftpd.conf <<-EOF
+		background=NO
+		listen_ipv6=NO
+		listen=YES
+		session_support=NO
+		anonymous_enable=NO
+		local_enable=YES
+		allow_writeable_chroot=YES
+		chroot_local_user=YES
+		guest_enable=NO
+		local_umask=022
+		passwd_chroot_enable=YES
+		use_localtime=YES
+		dirlist_enable=YES
+		dirmessage_enable=NO
+		hide_ids=YES
+		write_enable=YES
+		connect_from_port_20=NO
+		ftp_data_port=20
+		max_clients=0
+		max_per_ip=0
+		pasv_address=0.0.0.0
+		pasv_addr_resolve=YES
+		pasv_promiscuous=YES
+		pasv_enable=YES
+		pasv_max_port=21110
+		pasv_min_port=21100
+		port_enable=YES
+		seccomp_sandbox=NO
+		EOF
 	fi
 	log "Ftp Server init process done. Ready for start up."
 }
