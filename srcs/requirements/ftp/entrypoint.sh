8ff6106b49c259263dac0de80078dc7be9cf0475
Santi Guzman <sguzman@cbr13s4.42barcelona.com>
2025-07-28 19:59:52 +0200

    payaso

--- srcs/requirements/ftp/entrypoint.sh
+++ srcs/requirements/ftp/entrypoint.sh
@@ -19,43 +19,43 @@ verify_minimum_env() {
 
 ftp_init()
 {
-	if [ ! -d /home/$FTP_USER ]; then
-		addgroup -S "$FTP_USER" > /dev/null 2>&1
-		adduser -D -G "$FTP_USER" -h "/home/$FTP_USER" -s "/bin/false" "$FTP_USER" > /dev/null 2>&1
+	if ! getent passwd "$FTP_USER"; then
+		adduser --disabled-password "$FTP_USER"
 		mkdir -p /home/$FTP_USER
-		chown -R "$FTP_USER":"$FTP_USER" /home/$FTP_USER
+		echo "$FTP_USER" >> /etc/vsftpd/vsftpd.userlist
 		echo "$FTP_USER:$FTP_PASS" | chpasswd
+		chown -R "$FTP_USER":"$FTP_USER" /home/$FTP_USER
+		usermod --home /home/$FTP_USER "$FTP_USER"
 	fi
 	if [ ! -f /etc/vsftpd.conf ]; then
 		log "Configuring vsftpd"
 		cat > /etc/vsftpd.conf <<-EOF
-		background=NO
-		listen_ipv6=NO
 		listen=YES
-		session_support=NO
-		anonymous_enable=NO
+		listen_ipv6=NO
+		background=NO
 		local_enable=YES
-		allow_writeable_chroot=YES
-		chroot_local_user=YES
-		guest_enable=NO
-		local_umask=022
-		passwd_chroot_enable=YES
-		use_localtime=YES
-		dirlist_enable=YES
-		dirmessage_enable=NO
-		hide_ids=YES
+		local_root=/home/$FTP_USER
+		userlist_deny=NO
+		userlist_enable=YES
+		userlist_file=/etc/vsftpd/vsftpd.userlist
 		write_enable=YES
-		connect_from_port_20=NO
-		ftp_data_port=20
-		max_clients=0
-		max_per_ip=0
-		pasv_address=0.0.0.0
-		pasv_addr_resolve=YES
-		pasv_promiscuous=YES
 		pasv_enable=YES
-		pasv_max_port=21110
-		pasv_min_port=21100
+		pasv_min_port=5000
+		pasv_max_port=5010
+		anonymous_enable=NO
+		no_anon_password=NO
+		anon_upload_enable=NO
+		anon_mkdir_write_enable=NO
+		guest_enable=NO
+		chroot_local_user=YES
+		chroot_list_enable=NO
+		allow_writeable_chroot=YES
 		port_enable=YES
+		listen_port=21
+		listen_address=0.0.0.0
+		hide_ids=NO
+		dirmessage_enable=YES
+		pam_service_name=vsftpd
 		seccomp_sandbox=NO
 		EOF
 	fi
